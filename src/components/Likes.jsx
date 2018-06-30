93b1e3a8d106881d72c5ff4f955347c716fb45bd
Nicole Fish <fish@elevatesecurity.com>
2018-06-30 05:53:47 -0700

    Add exercise 4 instructions and solutions

--- src/components/Likes.jsx
+++ src/components/Likes.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 // Exercise 2: Set It Up
 // write a functional component
 // make it a div with a class name of Likes
@@ -9,16 +7,26 @@ import React from 'react';
 // Exercise 3: Prop It Up
 // Likes should take in number of likes as a prop and display it.
 
-const Likes = (props) => {
-	const { totalLikes } = props;
-	return (
-		<div className="Likes">
-			<span>{ totalLikes } likes</span>
-			<button className="LikesButton">
-				Like
-			</button>
-		</div>
-	);
+// Exercise 4: Class It Up
+// Turn Likes from a functional to a class component.
+	 // handle import/inheritance
+	 // add a render method
+	 // change props from function argument to object attribute
+
+import React, { Component } from 'react';
+
+class Likes extends Component {
+	render() {
+		const { totalLikes } = this.props;
+		return (
+			<div className="Likes">
+				<span>{ totalLikes } likes</span>
+				<button className="LikesButton">
+					Like
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Likes;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import data from '../../data.json';
-import ImageUploaderForm from '../components/ImageUploaderForm.jsx';
-import FeedList from '../components/FeedList';
-
 // Exercise 2: Set It Up
 // write a functional component
 // make it a div with a class name of HomePage
@@ -13,15 +8,26 @@ import FeedList from '../components/FeedList';
 // Exercise 3: Prop It Up
 // Need to extract photos from data and then pass it into HomePage
 
-const { photos } = data;
+// Exercise 4: Class It Up
+// Turn HomePage from a functional to a class component.
+	 // handle import/inheritance
+	 // add a render method
 
-const HomePage = () => {
-	return (
-		<div className="HomePage">
-			<ImageUploaderForm />
-			<FeedList photos={photos} />
-		</div>
-	);
+import React, { Component } from 'react';
+import data from '../../data.json';
+import ImageUploaderForm from '../components/ImageUploaderForm.jsx';
+import FeedList from '../components/FeedList';
+
+class HomePage extends Component {
+	render() {
+		const { photos } = data;
+		return (
+			<div className="HomePage">
+				<ImageUploaderForm />
+				<FeedList photos={photos} />
+			</div>
+		);
+	}
 }
 
 export default HomePage;
