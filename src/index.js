92459c929a113354a3c1f3bfdbdae77ddf5cab67
Nicole Fish <fish@elevatesecurity.com>
2018-06-30 06:19:39 -0700

    Standardize instruction comment/ import order for all components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
+// Exercise 1: Render It
+	// import React
+	// import ReactDOM
+	// import App
+	// import styles
+	// render App component into app-root element
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
 import './style.css';
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
-
-// Exercise 1: Render It
-// import React
-// import ReactDOM
-// import App
-// import styles
-// render App component into app-root element
