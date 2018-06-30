f0c7f9691ade76fc5aac3dfb91e9f4e87ff814e1
Nicole Fish <fish@elevatesecurity.com>
2018-06-30 06:25:41 -0700

    Standardize instruction comment/ import order for all components

--- src/components/FeedItem.jsx
+++ src/components/FeedItem.jsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import Likes from './Likes.jsx';
-
 // Exercise 2: Set It Up
-// write a functional component
-// make it a div with class name FeedItem
-// make an img element inside
-// import Likes, include it underneath img
-// export the component!
+	// write a functional component
+	// make it a div with class name FeedItem
+	// make an img element inside
+	// import Likes, include it underneath img
+	// export the component!
 
 // Exercise 3: Prop It Up
-// Need to pass in props as params to the component.
-// Need to destructure what you need from the props.
-// Hint: What do you need in order to get the image to display?
-// Hint what do you need in order to get likes to display in the Likes component?
+	// pass in props as params to the component
+	// destructure what you need from the props
+	// Hint: what do you need in order to get the image to display?
+	// Hint: what do you need in order to get likes to display in the Likes component?
+
+import React from 'react';
+import Likes from './Likes.jsx';
 
 const FeedItem = (props) => {
   const { photo } = props;
   const { url, likes } = photo;
   return (
     <div className="FeedItem">
-      <img className="Image" src={ url } />
+      <img className="Image" src={url} />
       <Likes totalLikes={likes} />
     </div>
   );
