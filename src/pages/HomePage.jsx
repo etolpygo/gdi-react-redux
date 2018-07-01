afa50704f84c21ccc53bba6eeff0c8c7d9e5e1e0
hyendler <hyendler@gmail.com>
2018-07-01 08:49:30 -0700

    Fix image uploader form

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,12 +19,31 @@ import ImageUploaderForm from '../components/ImageUploaderForm.jsx';
 import FeedList from '../components/FeedList';
 
 class HomePage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			photos: data.photos
+		}
+	}
+
+	handleAddPhoto = (url) => {
+		const newPhoto = {
+			url: url,
+			likes: 0
+		};
+		this.setState((prevState) => {
+			return {
+				photos: prevState.photos.concat([newPhoto])
+			};
+		});
+	}
+
 	render() {
 		const { photos } = data;
 		return (
 			<div className="HomePage">
-				<ImageUploaderForm />
-				<FeedList photos={photos} />
+				<ImageUploaderForm handleAddPhoto={this.handleAddPhoto} />
+				<FeedList photos={this.state.photos} />
 			</div>
 		);
 	}
