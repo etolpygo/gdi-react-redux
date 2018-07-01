05ff28b954f40d3935a6292a5c3a9d844b322287
Nicole Fish <fish@elevatesecurity.com>
2018-06-30 23:22:43 -0700

    bring in ImageUploaderForm from exercise 2

--- src/components/ImageUploaderForm.jsx
+++ src/components/ImageUploaderForm.jsx
@@ -1,7 +1,18 @@
 // Exercise 2: Set It Up
 	// write a functional component
 	// make it a form element with class name ImageUploaderForm
-	// write one input element and button element inside.
+	// write one input element and button element inside
 	// export the component!
 
 import React from 'react';
+
+const ImageUploaderForm = () => {
+	return(
+		<form className="ImageUploaderForm">
+			<input />
+			<button>Submit</button>
+		</form>
+	)
+}
+
+export default ImageUploaderForm;
