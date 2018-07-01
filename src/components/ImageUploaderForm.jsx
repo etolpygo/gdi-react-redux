d227e9aa57031c1037183de314ae9b98eb9d06a9
Nicole Fish <fish@elevatesecurity.com>
2018-07-01 02:06:07 -0700

    add form instructions and solution

--- src/components/ImageUploaderForm.jsx
+++ src/components/ImageUploaderForm.jsx
@@ -4,15 +4,50 @@
 	// write one input element and button element inside
 	// export the component!
 
-import React from 'react';
+// Exercise 6: Get InFormation
+// rewrite this component to accept user input
+	 // when the user inputs a url, the component should log it to the console
+	 // create internal state, to hold the user inputted value
+	 // render a form element containing:
+			// input element with a onChange attribute
+			// button with an onSubmit() attribute
+	 // write a handleChange() function to store the input
+	 // write a handleSubmit() function to print to the console
 
-const ImageUploaderForm = () => {
-	return(
-		<form className="ImageUploaderForm">
-			<input />
-			<button>Submit</button>
-		</form>
-	)
+import React, { Component } from 'react';
+
+class ImageUploaderForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ''
+    }
+  }
+
+	handleChange = (e) =>
+  	this.setState({ url: e.target.value });
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+  	console.log(this.state.url);
+  }
+
+	render() {
+		return (
+			<form className="ImageUploaderForm">
+
+				<input
+					placeholder="Add URL here"
+					value={this.state.url}
+					onChange={this.handleChange} />
+
+				<button className="SubmitButton" onClick={this.handleSubmit} >
+					Submit
+				</button>
+
+			</form>
+		)
+	}
 }
 
 export default ImageUploaderForm;
